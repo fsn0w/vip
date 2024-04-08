c3e0625998c923534b36ed5933dc11a87a05d1ba
IM <k.prytula@gmail.com>
2024-04-08 16:33:08 +0300

    upd

--- theme/templates/flexible/hero_banner.php
+++ theme/templates/flexible/hero_banner.php
@@ -9,11 +9,9 @@ $subtitle2   = get_sub_field( 'subtitle_2' );
 
 <div class="relative md:h-[200vh]">
 
-    <div class="fixed inset-0 w-full flex flex-wrap items-center text-prima-white font-mono font-medium uppercase bg-prima-red" x-data="{ scroll: false }" @scroll.window="scroll = (window.pageYOffset > 170) ? true : false">
+    <div class="md:fixed md:inset-0 w-full flex flex-wrap items-center text-prima-white font-mono font-medium uppercase bg-prima-red" x-data="{ scroll: false }" @scroll.window="scroll = (window.pageYOffset > 50) ? true : false">
 
-    <div class="w-full xl:w-1/2 bg-prima-white overflow-hidden">
-        <img src="<?=$image['url']?>" alt="" class="">
-    </div>
+    <div class="w-full xl:w-1/2 bg-prima-white overflow-hidden self-stretch bg-cover bg-no-repeat bg-top min-h-[50vh]" style="background-image:url('<?=$image['url']?>')">   </div>
 
     <div class="flex flex-col self-stretch py-20 min-h-48 items-center justify-center text-center w-full px-4 xl:w-1/2 overflow-hidden">
         <div class="max-w-96 transition-opacity duration-300 hide-up-animation" :class="{'opacity-0':scroll}">
