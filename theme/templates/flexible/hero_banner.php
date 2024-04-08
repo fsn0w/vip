6b5ee3b85d9a79f7109d0e23f69c8a4b07f4b619
IM <k.prytula@gmail.com>
2024-04-08 15:27:38 +0300

    upd

--- theme/templates/flexible/hero_banner.php
+++ theme/templates/flexible/hero_banner.php
@@ -16,11 +16,11 @@ $subtitle2   = get_sub_field( 'subtitle_2' );
     </div>
 
     <div class="flex flex-col self-stretch py-20 min-h-48 items-center justify-center text-center w-full px-4 xl:w-1/2 overflow-hidden">
-        <div class="max-w-96 hide-up-animation" :class="{'opacity-0':scroll}">
+        <div class="max-w-96 transition-opacity duration-300 hide-up-animation" :class="{'opacity-0':scroll}">
             <h1><?=$title?></h1>
             <span><?=$subtitle?></span>
         </div>
-        <div class="absolute max-w-96 opacity-0 show-up-animation" :class="{'opacity-100':scroll}">
+        <div class="absolute max-w-96 opacity-0 px-4 transition-opacity duration-300 show-up-animation" :class="{'opacity-100':scroll}">
             <span><?=$subtitle2?></span>
         </div>
     </div>
